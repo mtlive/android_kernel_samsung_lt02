c85bff8190cd6a1dfa18009b470c2c8d7a0f61d9
mtlive <m.t_79@hotmail.com>
2018-08-09 19:35:17 +0430

    Update gc_hal_kernel_os.c
    To get rid of:
    
    drivers/marvell/graphics/galcore_4x/hal/os/linux/kernel/gc_hal_kernel_os.c:9590:8: error: 'has_feat_separated_gc_power' is static but used in inline function '_ToEnablePower' which is not static [-Werror]
         if(has_feat_separated_gc_power())
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/marvell/graphics/galcore_4x/hal/os/linux/kernel/gc_hal_kernel_os.c:9534:8: error: 'has_feat_separated_gc_power' is static but used in inline function '_ToDisablePower' which is not static [-Werror]
         if(has_feat_separated_gc_power())
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/marvell/graphics/galcore_4x/hal/os/linux/kernel/gc_hal_kernel_os.c:9472:8: error: 'has_feat_separated_gc_clock' is static but used in inline function '_ToEnableClock' which is not static [-Werror]
         if(has_feat_separated_gc_clock())
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/marvell/graphics/galcore_4x/hal/os/linux/kernel/gc_hal_kernel_os.c:9416:8: error: 'has_feat_separated_gc_clock' is static but used in inline function '_ToDisableClock' which is not static [-Werror]
         if(has_feat_separated_gc_clock())
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~

--- drivers/marvell/graphics/galcore_4x/hal/os/linux/kernel/gc_hal_kernel_os.c
+++ drivers/marvell/graphics/galcore_4x/hal/os/linux/kernel/gc_hal_kernel_os.c
@@ -9404,7 +9404,7 @@ gckOS_ResetGPU(
     return gcvSTATUS_NOT_SUPPORTED;
 }
 
-gceSTATUS inline
+static gceSTATUS inline
 _ToDisableClock(
     IN gckOS Os,
     IN gceCORE Core,
@@ -9460,7 +9460,7 @@ _ToDisableClock(
     return gcvSTATUS_OK;
 }
 
-gceSTATUS inline
+static gceSTATUS inline
 _ToEnableClock(
     IN gckOS Os,
     IN gceCORE Core,
@@ -9522,7 +9522,7 @@ _ToEnableClock(
     return gcvSTATUS_OK;
 }
 
-gceSTATUS inline
+static gceSTATUS inline
 _ToDisablePower(
     IN gckOS Os,
     IN gceCORE Core,
@@ -9578,7 +9578,7 @@ _ToDisablePower(
 }
 
 
-gceSTATUS inline
+static gceSTATUS inline
 _ToEnablePower(
     IN gckOS Os,
     IN gceCORE Core,
